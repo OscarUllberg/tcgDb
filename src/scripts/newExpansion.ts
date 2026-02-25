454f3480e0929d8eab74219febedadadf13bd97e
OscarUllberg <oscar.ullberg@gmail.com>
2026-02-25 16:22:00 +0100

    Add SWSH8: Fusion strike, the rest of it

--- src/scripts/newExpansion.ts
+++ src/scripts/newExpansion.ts
@@ -4,8 +4,8 @@ import sortCardsById from "./sortCardsById";
 import sortCardsByName from "./sortCardsByName";
 
 const newExpansion = async () => {
-  const expansionCode = "sm12";
-  const expansionId = "CEC";
+  const expansionCode = "swsh8";
+  const expansionId = "FST";
 
   // read new set
   const newExpansionData = await fetch(`cardBySet/${expansionCode}.json`).then(
